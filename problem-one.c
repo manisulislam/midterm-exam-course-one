d3f83d32a0243c019fc7847ce2e9dc8655c06825
anisul islam <anisulislam980340@gmail.com>
2023-04-26 11:53:29 +0600

    problem one solved

---
+++ problem-one.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  int N;
+  scanf("%d", &N)        ;
+  int A[N];
+  for(int i=0; i<N; i++){
+    scanf("%d", &A[i]);
+  }
+  int even=0, odd=0;
+  for(int i=0; i<N; i++){
+    if(A[i]%2==0){
+        even++;
+    }
+    else{
+        odd++;
+    }
+  }
+  printf("%d %d ", even, odd);
+  return 0;
+}
